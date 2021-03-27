7f501e74b8dfbe1937c85eaddc7177bc4f7a67cc
yeomcda <>
2021-03-27 11:45:03 +0900

    movie 로 데이터베이스 변경

--- graphql/db.js
+++ graphql/db.js
@@ -1,25 +1,34 @@
-export const people = [
+const movies = [
   {
     id: 1,
-    name: "yeomc",
-    age: 33,
-    gender: "male",
+    name: "영화 1",
+    score: 4,
   },
   {
     id: 2,
-    name: "eomc",
-    age: 31,
-    gender: "female",
+    name: "영화 2",
+    score: 3,
   },
   {
     id: 3,
-    name: "kimc",
-    age: 30,
-    gender: "male",
+    name: "영화 3",
+    score: 5,
   },
 ];
 
+export const getMovies = () => movies;
+
 export const getById = (id) => {
-  const filteredPeople = people.filter((person) => person.id === id);
-  return filteredPeople[0];
+  const filteredMovies = movies.filter((movie) => movie.id === id);
+  return filteredMovies[0];
+};
+
+export const deleteMovie = (id) => {
+  const cleanedMovies = movies.filter((movie) => movie.id !== id);
+  if (movies.length > cleanedMovies.length) {
+    movies = cleanedMovies;
+    return true;
+  } else {
+    return false;
+  }
 };
